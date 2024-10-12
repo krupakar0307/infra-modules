ffad9071aac762a4b455f75b9324f4e6692318bc
krupakar reddy <krupakarreddy.yasa@gmail.com>
2024-10-12 17:24:19 +0530

    add output

---
+++ vpc/output.tf
@@ -0,0 +1,11 @@
+output "vpc_id" {
+  value = aws_vpc.vpc-dev.id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.vpc-dev.cidr_block
+}
+
+output "aws_private_subnets" {
+  value = aws_subnet.private-subnets[*].cidr_block
+}
